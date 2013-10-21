803abaf07e4ed14d803a6e8d4ba173eee476b946
kyoendo <postagie@gmail.com>
2013-10-21 15:05:18 +0900

    Add a test for F#path

--- spec/dir_friend_spec.rb
+++ spec/dir_friend_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 describe DirFriend do
+  include FakeFS::SpecHelpers
   it 'should have a version number' do
     DirFriend::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe DirFriend::F do
+    before(:each) do
+      File.write('foo.txt', '')
+      @f = DirFriend::F.new('foo.txt')
+    end
+
+    describe '#path' do
+      it 'returns a file path' do
+        path = File.expand_path('foo.txt')
+        expect(@f.path).to eq path
+      end
+    end
   end
 end
