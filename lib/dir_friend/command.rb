fd646140d125fda14f36e5d0e21e7fd5cc179154
kyoendo <postagie@gmail.com>
2013-10-28 13:11:17 +0900

    Add dot command

--- lib/dir_friend/command.rb
+++ lib/dir_friend/command.rb
@@ -3,12 +3,28 @@ require 'thor'
 module DirFriend
   class Command < Thor
     
-    desc "info DIR", "Show DIR info"
+    desc "info PATH", "Show PATH info"
     def info(path)
       puts Any.new(path).info
     end
 
-
+    desc "dot PATH [FORMAT]", "Build & save a graphviz dot file"
+    option :layout, aliases:"-l"
+    option :colorscheme, aliases:"-c"
+    option :dir_shape, aliases:"-d"
+    option :file_shape, aliases:"-f"
+    option :global, aliases:"-g"
+    option :nodes, aliases:"-n"
+    option :edges, aliases:"-e"
+    option :save, aliases:"-s", default:'a'
+    option :depth, default:9
+    def dot(path)
+      opt = options.dup.inject({}) { |h, (k,v)| h[k.intern] = v; h  }
+      save_path = opt.delete(:save)
+      opt = opt_parser(opt)
+      dir = D.new(path, options[:depth])
+      dir.to_dot(opt).save(save_path)
+    end
 
     desc "version", "Show DirFriend version"
     def version
@@ -26,5 +42,19 @@ DirFriend is a friend of file directory.
     end
     default_task :banner
     map "-h" => :banner
+  end
+
+  no_commands do
+    def opt_parser(opt)
+      %i(global nodes edges).each do |attr|
+        if kv = opt.delete(attr)
+          kv_arr = kv.split(/\s*,\s*/)
+                     .map{ |kv| kv.split(/\s*:\s*/).map(&:strip) }
+                     .map{ |k, v| [k.intern, v] }
+          opt.update({attr => Hash[ kv_arr ]})
+        end
+      end
+      opt
+    end
   end
 end
