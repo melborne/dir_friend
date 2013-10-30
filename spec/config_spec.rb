ed34723ad021685f83667d9f2101742b4d22a324
kyoendo <postagie@gmail.com>
2013-10-30 23:25:31 +0900

    Add config tests

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'fakefs/spec_helpers'
 
+
 describe DirFriend::Config do
   include FakeFS::SpecHelpers
 
@@ -9,17 +10,72 @@ describe DirFriend::Config do
     File.write(described_class::CONFIG_PATH, ~<<-EOS)
       default: :theme1
       theme1:
-        color: reds
-        shape: box
+        colorscheme: reds
+        layout: fdp
+      theme2:
+        colorscheme: blues
+        layout: dot
     EOS
   end
 
-  describe '.themes' do
+  describe '#themes' do
     context 'config file exist' do
       it 'read user settings' do
-        themes = described_class.send(:themes)
-        expected = {default: :theme1, theme1:{color:'reds', shape:'box'}}
+        themes = described_class.new.themes
+        expected = { default: :theme1,
+                     theme1:{colorscheme:'reds', layout:'fdp'},
+                     theme2:{colorscheme:'blues', layout:'dot'} }
         expect(themes).to eq expected
+      end
+    end
+
+    context 'config file not exist' do
+      it 'returns a empty hash' do
+        described_class.new.instance_variable_set("@themes", nil)
+        stub_const("DirFriend::Config::CONFIG_PATH", "wrong_name_config.yaml")
+        themes = described_class.new.themes
+        expect(themes).to be_empty
+      end
+    end
+  end
+
+  describe '#build' do
+    context 'with nil argument' do
+      it 'returns default theme' do
+        theme = described_class.build(nil)
+        expected = {colorscheme:'reds', layout:'fdp'}
+        expect(theme).to eq expected
+      end
+    end
+
+    context 'with a theme argument' do
+      it 'returns the theme' do
+        theme = described_class.build(:theme2)
+        expected = {colorscheme:'blues', layout:'dot'}
+        expect(theme).to eq expected
+      end
+    end
+
+    context 'when the default value is a hash data' do
+      before(:each) do
+        File.write(described_class::CONFIG_PATH, ~<<-EOS)
+          default:
+            layout: fdp
+            graph:
+              splines: ortho
+          theme1:
+            colorscheme: reds
+            layout: fdp
+          theme2:
+            colorscheme: blues
+            layout: dot
+        EOS
+      end
+
+      it 'returns the default value' do
+        theme = described_class.build(nil)
+        expected = {layout:'fdp', graph:{splines:'ortho'}}
+        expect(theme).to eq expected
       end
     end
   end
